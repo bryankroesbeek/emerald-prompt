c2e48f111bc71ef6707be1d101aeb4a213f2e9c5
Bryan <bryank.b96@gmail.com>
2024-06-22 16:06:22 +0200

    Move status code to apprioprate file, and add BUG note

--- git/git.go
+++ git/git.go
@@ -1,29 +1,9 @@
 package git
 
 import (
-	"fmt"
-	"os/exec"
 	"strings"
 )
 
-func GetStatus() string {
-	var cmd = exec.Command("git", "status", "-z")
-	var res, err = cmd.Output()
-	if err != nil {
-		return ""
-	}
-
-	var changesStr = string(res)
-	var changes = strings.Split(changesStr, "\000")
-	var countI, countW = countChanges(changes)
-	fmt.Println(countI, countW)
-
-	if len(res) == 0 {
-		return ":)"
-	}
-
-	return ":("
-}
 
 func GetBranch() string {
 	var branch, err = getBranchName()
--- git/status.go
+++ git/status.go
@@ -1,5 +1,31 @@
 package git
 
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+)
+
+// BUG: When a rename is in the status, it will show up as both a tracked and untracked change
+func GetStatus() string {
+	var cmd = exec.Command("git", "status", "-z")
+	var res, err = cmd.Output()
+	if err != nil {
+		return ""
+	}
+
+	var changesStr = string(res)
+	var changes = strings.Split(changesStr, "\000")
+	var countI, countW = countChanges(changes)
+	fmt.Println(countI, countW)
+
+	if len(res) == 0 {
+		return ":)"
+	}
+
+	return ":("
+}
+
 func countChanges(changes []string) (int, int) {
 	var index int = 0
 	var workingTree int = 0
