f9d04bdcee60798b76a85c6bd69ede60a1589143
Bryan <bryank.b96@gmail.com>
2024-06-22 10:06:03 +0200

    Get and count changes

--- git/tools.go
+++ git/tools.go
@@ -15,12 +15,44 @@ func getBranchName() (string, error) {
 	return string(res), nil
 }
 
+func countChanges(changes []string) (int, int) {
+	var index int = 0
+	var workingTree int = 0
+
+	for _, change := range changes {
+		if change == "" {
+			continue
+		}
+		var cIndex = string(change[0])
+		var tIndex = string(change[1])
+
+		if string(change[0:2]) == "??" {
+			workingTree = workingTree + 1
+			continue
+		}
+
+		if cIndex != " " {
+			index = index + 1
+		}
+		if tIndex != " " {
+			workingTree = workingTree + 1
+		}
+	}
+
+	return index, workingTree
+}
+
 func GetStatus() string {
-	var cmd = exec.Command("git", "status", "--porcelain")
+	var cmd = exec.Command("git", "status", "-z")
 	var res, err = cmd.Output()
 	if err != nil {
 		return ""
 	}
+
+	var changesStr = string(res)
+	var changes = strings.Split(changesStr, "\000")
+	var countI, countW = countChanges(changes)
+	fmt.Println(countI, countW)
 
 	if len(res) == 0 {
 		return ":)"
