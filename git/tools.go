cf56a146ffa6e57b1e53f47c3b0558be4de2d156
Bryan <bryank.b96@gmail.com>
2024-06-22 10:07:13 +0200

    Get and count commit differences

--- git/tools.go
+++ git/tools.go
@@ -1,8 +1,10 @@
 package git
 
 import (
+	"errors"
 	"fmt"
 	"os/exec"
+	"slices"
 	"strings"
 )
 
@@ -40,6 +42,31 @@ func countChanges(changes []string) (int, int) {
 	}
 
 	return index, workingTree
+}
+
+func countCommits(source string, target string) (int, error) {
+	var aheadCmd = exec.Command("git", "rev-list", fmt.Sprint(source, "..", target))
+	var res, err = aheadCmd.Output()
+	if err != nil {
+		return 0, err
+	}
+
+	var revList = strings.Split(string(res), "\n")
+	revList = slices.DeleteFunc(revList, func(e string) bool { return e == "" })
+
+	return len(revList), nil
+}
+
+func getCommitCounts(branchName string) (int, int, error) {
+	var origin = "origin/" + branchName
+
+	var commitsAhead, aErr = countCommits("HEAD", origin)
+	var commitsBehind, bErr = countCommits(origin, "HEAD")
+	if aErr != nil || bErr != nil {
+		return 0, 0, errors.New("Unable to get commits")
+	}
+
+	return commitsBehind, commitsAhead, nil
 }
 
 func GetStatus() string {
