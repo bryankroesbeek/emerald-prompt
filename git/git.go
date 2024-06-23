637cb1361689dca2431c80e7e41018440d4fe588
Bryan <bryank.b96@gmail.com>
2024-06-23 12:18:59 +0200

    Get git sync status

--- git/git.go
+++ git/git.go
@@ -2,8 +2,30 @@ package git
 
 import (
 	"strings"
+	"emerald/colors"
 )
 
+type SyncStatus struct {
+	status GitStatus
+	icon   string
+	color  func(string, bool) string
+}
+
+func getSyncStatus(ahead int, behind int) SyncStatus {
+	if ahead > 0 && behind > 0 {
+		return SyncStatus{Diverged, GitIconDiverged, colors.Yellow}
+	}
+
+	if ahead > 0 {
+		return SyncStatus{Ahead, GitIconAhead, colors.Green}
+	}
+
+	if behind > 0 {
+		return SyncStatus{Behind, GitIconBehind, colors.Red}
+	}
+
+	return SyncStatus{Equal, GitIconEqual, colors.Cyan}
+}
 
 func GetBranch() string {
 	var branch, err = getBranchName()
